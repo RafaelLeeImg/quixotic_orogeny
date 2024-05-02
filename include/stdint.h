f76c9b608322be44cd5a68bb1a3b5828d0da16ed
Rafael Lee <rafaellee.img@gmail.com>
2024-05-02 18:09:29 +0900

    Add stdint.h

---
+++ include/stdint.h
@@ -0,0 +1,12 @@
+#ifndef __STDINT_H__
+#define __STDINT_H__
+
+typedef unsigned char uint8_t;
+typedef unsigned short uint16_t;
+typedef unsigned int uint32_t;
+
+typedef signed char int8_t;
+typedef signed short int16_t;
+typedef signed int int32_t;
+
+#endif /* __STDINT_H__ */

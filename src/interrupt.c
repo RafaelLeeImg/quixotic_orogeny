d2c7a29c1e9b56c78904b8af2a5a4a15ada6821b
Rafael Lee <rafaellee.img@gmail.com>
2024-05-10 08:12:24 +0900

    Add a counter variable in sys_tick_handler

--- src/interrupt.c
+++ src/interrupt.c
@@ -1,7 +1,10 @@
-
 void cm_disable_interrupts(void) { __asm__ volatile("CPSID I\n"); }
 void cm_disable_faults(void) { __asm__ volatile("CPSID F\n"); }
 void cm_enable_interrupts(void) { __asm__ volatile("CPSIE I\n"); }
 void cm_enable_faults(void) { __asm__ volatile("CPSIE F\n"); }
 
-void sys_tick_handler(void) { print("sys_tick_handler\n"); }
+volatile unsigned int systick_cnt = 0;
+
+void sys_tick_handler(void) {
+  systick_cnt++;
+}

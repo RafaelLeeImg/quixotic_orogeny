0ed03932cb37e545567f6fbb9fa0890e87021a9b
Rafael Lee <rafaellee.img@gmail.com>
2024-05-12 10:38:12 +0900

    Modify spinlock test cases to mutex

--- src/interrupt.c
+++ src/interrupt.c
@@ -1,3 +1,6 @@
+#include "mutex.h"
+#include "spinlock.h"
+
 void cm_disable_interrupts(void) { __asm__ volatile("CPSID I\n"); }
 void cm_disable_faults(void) { __asm__ volatile("CPSID F\n"); }
 void cm_enable_interrupts(void) { __asm__ volatile("CPSIE I\n"); }
@@ -5,6 +8,28 @@ void cm_enable_faults(void) { __asm__ volatile("CPSIE F\n"); }
 
 volatile unsigned int systick_cnt = 0;
 
+extern pthread_mutex_t lock1;
+
 void sys_tick_handler(void) {
+  // extern unsigned int systick_cnt;
+  // print(".");
+  if ((systick_cnt % 10) == 0) {
+    print(".");
+    if ((systick_cnt % 1000) == 0) {
+      print("\n");
+    }
+  }
+
+  if ((systick_cnt % 500) == 305) {
+    printf("t = %ud\n", systick_cnt);
+    print("305 before unlock\n");
+    // __clrex();
+    printf("lock1.locked_ = %ud\n", lock1.locked_);
+    // pthread_spin_lock(&lock1);
+    pthread_mutex_unlock(&lock1);
+    print("305 after unlock\n");
+    printf("lock1.locked_ = %ud\n", lock1.locked_);
+  }
+
   systick_cnt++;
 }
--- src/main.c
+++ src/main.c
@@ -3,6 +3,9 @@
 #include "hardware_gpio.h"
 #include "hardware_systick.h"
 #include "hardware_usart.h"
+#include "mutex.h"
+#include "spinlock.h"
+#include "stdio.h"
 #include "string.h"
 
 #define MAX_STRING_LEN 4096
@@ -12,6 +15,8 @@ void print(char const str[]) {
     *USART1_DR = str[i];
   }
 }
+
+pthread_mutex_t lock1;
 
 int main(void) {
 
@@ -27,6 +32,44 @@ int main(void) {
   cm_enable_interrupts();
 
   cm_enable_faults();
+
+  printf("console_setup\n");
+  console_setup();
+
+  // extern static volatile unsigned int systick_cnt;
+  // extern systick_cnt;
+  // int systick_cnt = 0;
+  // extern lock1;
+  extern volatile unsigned int systick_cnt;
+  int locked = 0;
+  while (1) {
+    // printf(".");
+    if (systick_cnt % 500 == 10 /*  && systick_cnt <= 20 */) {
+      printf("t = %ud\n", systick_cnt);
+      pthread_mutex_init(&lock1, 0);
+    } else if (systick_cnt % 500 == 200 /*  && systick_cnt <= 210 */) {
+      printf("t = %ud\n", systick_cnt);
+      if (1) {
+        // if (locked == 0) {
+        print("200 before lock\n");
+        // __clrex();
+        printf("lock1.locked_ = %ud\n", lock1.locked_);
+        pthread_mutex_lock(&lock1);
+        // pthread_mutex_unlock(&lock1);
+        print("200 after lock\n");
+        printf("lock1.locked_ = %ud\n", lock1.locked_);
+        locked = 1;
+      }
+    } else if (systick_cnt % 500 == 499 /*  && systick_cnt <= 410 */) {
+      printf("t = %ud\n", systick_cnt);
+      if (locked == 1) {
+        printf("499 before unlock\n");
+        pthread_mutex_unlock(&lock1);
+        printf("499 after unlock\n");
+        locked = 0;
+      }
+    }
+  }
 
   for (int j = 0; j < 3000; j++) {
     ;
